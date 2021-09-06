45e9d4ae370c8450f8ec5913903c9dd5d57dab3e
xander <karimi.xander@gmail.com>
2021-09-06 11:20:17 +0430

    write logs to mongodb

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
 // const { query } = require('express');
-const express =require ('express')
-const morgan=require('morgan')
-require('express-async-errors')
-const winston= require('winston')
-const mongoose= require('mongoose');
-const {myMiddalware}= require('./middalware')
-const custommerRoutes= require ('./routes/CustommerRoutes')
-const userRoutes= require('./routes/userRoutes');
-const errorMiddleware = require('./middleware/errorMiddleware');
+const express = require("express");
+const morgan = require("morgan");
+require("express-async-errors");
+const winston = require("winston");
+require("winston-mongodb");
+const mongoose = require("mongoose");
+const { myMiddalware } = require("./middalware");
+const custommerRoutes = require("./routes/CustommerRoutes");
+const userRoutes = require("./routes/userRoutes");
+const errorMiddleware = require("./middleware/errorMiddleware");
 
 // console.log(config.get("databaseAddress"));
 
-
 // const {validation} = require('./data_validation')
 
-const app=express();
+const app = express();
 
+winston.add(
+  new winston.transports.File({ filename: "error.log", level: "error" })
+);
+winston.add(new winston.transports.MongoDB({ 
+    db:"mongodb://localhost:27017/logger"
+}))
 
-winston.add(new winston.transports.File({ filename: 'error.log', level: 'error' }))
+app.use(express.json());
+app.use(myMiddalware);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(custommerRoutes);
+app.use(userRoutes);
+if (app.get("env") === "development") app.use(morgan("tiny"));
 
-app.use(express.json())
-app.use(myMiddalware)
-app.use(express.urlencoded({ extended:true }))
-app.use(express.static("public"))
-app.use(custommerRoutes)
-app.use(userRoutes)
-if (app.get("env")==="development")
-app.use(morgan('tiny'))
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
 
-app.get('/', (req, res) => {
-     res.send("Hi")
-})
+app.use(errorMiddleware);
 
-app.use(errorMiddleware)
-
-mongoose.connect("mongodb://localhost:27017/custommer",{
+mongoose
+  .connect("mongodb://localhost:27017/custommer", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("database connected")
-}).catch(err => console.log(err))
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => console.log(err));
 
-const port=process.env.PORT || 3000
-app.listen(port,(err) => {
-    if (err) console.error(err)
-    else console.log(`app is listenning on port ${port}`)
-})
+const port = process.env.PORT || 3000;
+app.listen(port, (err) => {
+  if (err) console.error(err);
+  else console.log(`app is listenning on port ${port}`);
+});
